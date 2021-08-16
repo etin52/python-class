8f7047a9ac61750f1b869af7864fc3436789a97c
etin52 <oronsayeetin@gmail.com>
2021-08-16 23:31:42 +0800

    strings methods

---
+++ strings.py
@@ -0,0 +1,54 @@
+#  str1="this is john"
+# print(str1.title())
+# print(str1.upper())
+
+# print(str1.startswith("t"))
+# print(str1.endswith("n"))
+# print(str1.index("j"))
+# print(str1.find("z"))
+
+# str2=" "
+# print(str2.isspace())
+# list_of_word=str1.split("")
+
+# joined_words =" ".join(list_of_words)  
+# print(joined_words)
+
+# print("welcome")
+# print("Add two numbers together")
+# num1=float(input("Enter the first number\n"))
+# num2= float(input("Enter seccond number\n"))
+# print(num1+num2)
+
+
+# 1. wrtie a program that get input from a user and convert it to 
+# upper case
+                    # i.
+# string=input("convert to upper case\n")
+# print(string.upper())
+
+#                 # ii.
+# string=input("my name is joe\n")
+# print(string.title())
+  
+  
+#         #    iii.
+# string=input("Enter a sentence")
+# print(string.split(" "))
+
+
+                # iv.
+string1=float(input("input number one\n"))
+string2=float(input("input number two\n"))
+string3=float(input("input number three\n"))
+
+average=(string1+string2+string3)/3
+print(average)
+
+
+
+
+
+
+
+
