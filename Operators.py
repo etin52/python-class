56754d128fd4c22b970d70885ec51a36c8fe2f27
etin52 <oronsayeetin@gmail.com>
2021-08-16 18:32:29 +0800

    initial commit

---
+++ Operators.py
@@ -0,0 +1,147 @@
+#11-08-21
+
+# a=10
+# b=15
+
+# print(a==b) #check for quality
+# print(a!=b) #check for inequality
+# print(a>b) # check that a is greater than b
+# print(a<b) # check that a is less than b
+# print(a>=b) # check if a is greater or equal to b
+# print(a<=b) # check if a is less or equal to b
+
+
+# sentence = "this is is good class"
+# print("a" in sentence)
+# print("z" not in sentence)
+# print("z" in sentence)
+
+# STRINGS # to concatanation
+
+# print("hello"+"world")
+# print("hello" + "world")
+# print("hello" + " world")
+
+# # INDEXING 
+
+# string="continental"
+# print(string[6]) # to check for index
+# print(string[-5]) #to check for neg index
+
+# new_string ="blue sky"
+# print(new_string[4:])
+# print(new_string[5:])
+
+# my_string="sentence"
+# print(my_string[0:5])
+# print(my_string[0:5:2])
+# print(my_string[0:5:3])
+
+# class work
+
+# string="jhonDipPeta" # print Dip
+# print(string [4:7])
+
+# string="JaSonAy" # print son
+# print(string[2:5]) 
+
+# my_string= "housemates"   #print mate # print house # print neg index # skip 1 letter to get osm 
+# print(my_string[5:9])
+# print(my_string[1:5])
+# print(my_string[-9:-5])
+# print(my_string[-5:-1])
+# print(my_string[1:6:2])
+
+#_-----Formartting String----------
+# a=4
+# b=5
+
+# sentence=f"the sum of {a} and {b} is {a+b}"
+# print(sentence)
+
+
+# our_string="Some string"
+# section=our_string[:4]
+# print(f"{section} is a subset of '{our_string}'")
+
+#  print('Dear sir ,\nthis is lulus letter.\n\t\tYour\'s faitfully.\n\t\t lulu')
+
+
+
+#  print("""
+
+# Dear sir .
+
+# this is lulu he's doing ok
+
+ 
+#                 Your's faithfully
+#                 lulu paris
+#""")
+
+# string="w3resource" #print w3ce
+# one=string[:2]
+# two=string[8:]
+# print(one+two)
+
+
+# Assignment 
+
+# cal the area a trapezium 
+
+base = 5
+height= 5
+base_2= 6 
+a=6
+b=5
+h=5 
+numerator=(a+b)*h
+denominator= 2
+area = numerator/denominator
+print(area)
+
+
+# write a python program 
+word = "house hold"
+my_string ="house"
+new_string= "hold"
+print(my_string + new_string)
+print(my_string[0:2])
+print(new_string[5:7])
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ first-class.py
@@ -0,0 +1,212 @@
+# h = 15
+# print(h)
+
+# ############### DATA TYPES #############
+
+# float_data = 300.456   
+# type_of_data = type(float_data)
+# print(float_data)
+# print(type_of_data)
+
+# # ###### For A string DATA#####
+
+# string_data ="30"
+# mode_of_data = type(string_data)
+# print(string_data)
+# print(mode_of_data)
+
+# # ####### Integers #########
+# integer_data= 30
+# type_of_data=type(integer_data)
+# print(integer_data)
+# print(type_of_data)
+
+
+# #### BOOLEAN ######
+
+# a_boolean = True 
+# print(type(a_boolean))
+
+################3 Arithemetic OPERATIONS ################
+
+# find the value of y if the equation is y=2x=3**x
+
+
+# x =4**(-1)
+# print(x)
+
+# y = 2*x + 3**x
+# print(y)
+
+#########   Class Work ###########
+
+# A boy is 20yrs old and the father is older by 25yr old .
+# find the age of the father if the boy is now 25yrs old.
+
+# age_diff = 25
+# boy = 25
+
+#          # Solution
+
+
+# # calculate the interest of univelcity if they invested 20,000
+# # in 6 months at 20% per anum.
+#         # soln
+# # uni_investment= 20000
+# # time= 0.5
+# # rate= 0.2
+
+# # s_i =uni_investment * rate* time 
+# # print(s_i)
+
+ 
+#   ##### ASSIGNMENT OPRS ######
+# num = 3
+# print(num)
+# num +=1
+# print(num)
+
+# num=7
+# print(num)
+# num *=2
+# print(num)
+
+
+# ######### data changing type #########
+# # 1. 
+# my_string = "true"
+# to_bool = bool(my_string) # does not work
+
+# my_string = "400"
+# to_int = int(my_string)
+# print(type(to_int))   # it works
+
+
+# my_string = "5.5"
+# to_float = float(my_string)
+# print(type(to_float))
+
+# my_int = 20
+# in_float = float (my_int)
+# print(type(in_float))
+
+# my_int = 0
+# on_bool= bool(my_int)
+# print(on_bool)
+
+# my_float = 0
+# as_int = int(my_float)
+# print(type(as_int))
+
+# boolean = False 
+# bool_to_int= int(boolean)
+# print(bool_to_int)
+
+# my_float = 30.5
+# to_str =str(my_float)
+# print(type(to_str))
+
+
+######### class work #######
+
+ # 1.write a program that converts $20 to naira given that 
+  # $1 = #25 , leave your ans in float
+
+  # 2.Convert 50 imches to millimeters given dat 1 inch = 10.74 
+  # leave your ans in int.
+
+  # 3. find a yr dat a man was born if he is already 63 yrs of age
+
+  #4. cal the profit if blesing bought a shoe for for a 100naira and sold for $30 
+  # N:B $1 = 15
+
+# 5. find x if x= -b                              
+# 
+# a=4
+# b=3.2
+# c=7 
+
+                     
+                    # Soln.
+#if $1 = #25
+#we have $20 * 25 =#500  
+# 
+#exchange_rate = 25
+#dollar_value=20 
+
+
+#converted = exchange_rate * dollar_value 
+#to_float = float(converted)
+#print(to_float)
+
+#one_inche= 10.74
+#transfer_value=50 
+
+#convert = one_inche * transfer_value 
+#to_int = int(convert)
+#print(to_int)
+
+#current_year =2021 
+#man_age= 63
+
+#year_of_birth= current_year - man_age
+#print(year_of_birth)
+
+
+#dollar_value =30
+
+#selling_price=exchange_rate * dollar_value 
+#
+#profit_value= selling_price - naira_value 
+
+#--------- soln-------------
+
+
+
+
+
+
+
+#a=4
+# b=3.2
+# c=7 
+
+#numerator= -b + ((b**2) + (4*a*c))**0.5
+#denominator = 2*a
+
+# x=numerator/denominator 
+# print(x)
+
+
+# numerator= -b - ((b**2) + (4*a*c))**0.5
+# denominator = 2*a
+
+# x=numerator/denominator 
+# print(x)
+
+
+######## sold ###########
+
+# a=4
+# b=3
+# numerator = a**(-1+4)
+# denominator = 3*(4**-b)
+# x = numerator/ denominator 
+# print(x)
+
+
+# assignment no2
+
+a=2.5
+c=0.1
+numerator = (6**0.5)*4*a*c
+denominator = 3**(2*a)+ 4
+x=numerator/denominator 
+print(x)
+
+
+
+
+
+
+
