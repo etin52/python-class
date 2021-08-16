19c35c9cdfa4029743e8264ea12ad5e955e85eac
etin52 <oronsayeetin@gmail.com>
2021-08-16 19:25:36 +0800

    assignment corrections

---
+++ correction_2.py
@@ -0,0 +1,18 @@
+# write a python programto a single string from _typeshed import SupportsLenAndGetItem
+# from two given string separated by space.
+
+# Soln 
+string_1="ABC"
+string_2="XYZ"
+print(string_1+string_2)
+print(string_1 +" "+string_2)
+
+
+# cal the area of a trapezium
+
+h=5
+a=5
+b=6
+
+area=((a+b)*h)/2
+print(area)
