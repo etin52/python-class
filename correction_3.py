7cb2677f5c10e9dbeeb5fe2bcbcf525137f1b5a5
etin52 <oronsayeetin@gmail.com>
2021-08-18 05:35:18 +0800

    correction_3.py

---
+++ correction_3.py
@@ -0,0 +1,29 @@
+#Write a program that takes 3 numbers from the user and computes the average
+
+string1=input("enter a fisrt numner\n")
+string2= input("enter a seconnd numner\n")
+string3=input("enter a third number\n")
+
+average=input(string1+string2+string3)/3
+print(average)
+
+
+# Write a program that takes a sentence from the user and change the first word to upper case.
+
+string="never judge a book by its cover"
+print(string.title)
+
+# Write a program with the sentence I am learning python. When your program is run, the string I should be changed to you
+
+string=("i am learning python")
+print(string.replace("i" , "you"))
+
+# Write a program that takes the string "I hope you had fun today in class". Print the number of times that the 
+#string "a" appears in the sentence.
+string1=("i hope you had fun in class")
+print(string1.count("a"))
+
+
+
+
+
