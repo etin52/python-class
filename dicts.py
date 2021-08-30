87d0c8a24a826a20e14186be739a19a931f29f54
etin52 <oronsayeetin@gmail.com>
2021-08-30 17:57:49 +0100

    dicts.py

---
+++ dicts.py
@@ -0,0 +1,50 @@
+sampleDict = {
+"name": "Kelly",
+"age":25,
+"salary": 8000,
+"city": "New york"}
+
+sampleDict.pop("name","salary")
+print(sampleDict)
+
+# Rename key "city" to "location" in the following dictionary
+sampleDict= {
+"name":"Kelly",
+"age":25,
+"salary": 8000,
+"city":"New york"}
+
+sampleDict = ("location" "new york")
+print(sampleDict)
+
+
+
+# Access the value of key ‘history’ from the below 
+
+dictsampleDict = { 
+   "class":{ 
+      "student":{ 
+         "name":"Mike",
+         "marks":{ 
+            "physics":70,
+            "history":80
+         }
+      }
+   }
+}
+
+score=dictsampleDict['class']['student']['marks']['history']
+print(score)
+
+
+
+# # # Change Brad’s salary to 8500 from a given Python 
+# sampleDict = {
+# 'emp1': {'name': 'Jhon', 'salary': 7500}
+# 'emp2': {'name': 'Emma', 'salary': 8000}
+# 'emp3': {'name': 'Brad', 'salary': 6500} 
+# }
+
+# sampleDict=['emp3']['name':'Brad','salary':8500]
+# print(sampleDict)
+
